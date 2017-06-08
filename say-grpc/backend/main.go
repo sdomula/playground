6d205abf8f2cd3a877a313a5b4042242d4d81d10
Stan Domula <stan.domula@smartrac-group.com>
2017-06-08 23:00:23 +0200

    fix args usage

--- say-grpc/backend/main.go
+++ say-grpc/backend/main.go
@@ -1,16 +1,58 @@
 package main
 
 import (
-	"log"
-	"os"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net"
 	"os/exec"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	"github.com/Sirupsen/logrus"
+	pb "github.com/sdomula/playground/say-grpc/api"
 )
 
 func main() {
-	cmd := exec.Command("flite", "-t", os.Args[1], "-o", "output.wav")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	if err := cmd.Run(); err != nil {
-		log.Fatal(err)
+	port := flag.Int("p", 8080, "port to listen to")
+	flag.Parse()
+
+	logrus.Infof("listening to port %d", *port)
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		logrus.Fatalf("could not listen to port %d: %v", *port, err)
 	}
+
+	s := grpc.NewServer()
+	pb.RegisterTextToSpeachServer(s, server{})
+
+	if err := s.Serve(lis); err != nil {
+		logrus.Fatalf("could not server: %v", err)
+	}
+}
+
+type server struct{}
+
+func (server) Say(ctx context.Context, text *pb.Text) (*pb.Speech, error) {
+	f, err := ioutil.TempFile("", "")
+	if err != nil {
+		return nil, fmt.Errorf("could not create tmp file: %v", err)
+	}
+
+	if err := f.Close(); err != nil {
+		return nil, fmt.Errorf("could not close %s: %v", f.Name(), err)
+	}
+
+	cmd := exec.Command("flite", "-t", text.Text, "-o", f.Name())
+	if data, err := cmd.CombinedOutput(); err != nil {
+		return nil, fmt.Errorf("flite failed: %s", data)
+	}
+
+	data, err := ioutil.ReadFile(f.Name())
+	if err != nil {
+		return nil, fmt.Errorf("could not read tmp file: %v", err)
+	}
+
+	return &pb.Speech{Audio: data}, nil
 }
