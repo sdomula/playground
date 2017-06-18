f3d886f03e7d2f1650253967bc1b72cb7439b7cf
Stan Domula <sdomula@posteo.de>
2017-06-18 22:35:26 +0200

    add bench and optimize

--- alg/sort/bubble.go
+++ alg/sort/bubble.go
@@ -1,21 +1,18 @@
 package sort
 
 func bubble(s []int) []int {
-	for i := 0; i < len(s); i++ {
-		if !bubbleLoop(s, i) {
+	var swapped bool
+	for x := 0; x < len(s); x++ {
+		swapped = false
+		for i := 0; i+1 < len(s)-x; i++ {
+			if s[i] > s[i+1] {
+				s[i], s[i+1] = s[i+1], s[i]
+				swapped = true
+			}
+		}
+		if !swapped {
 			break
 		}
 	}
 	return s
-}
-
-func bubbleLoop(s []int, prev int) bool {
-	swapped := false
-	for i := 0; i+1 < len(s)-prev; i++ {
-		if s[i] > s[i+1] {
-			s[i], s[i+1] = s[i+1], s[i]
-			swapped = true
-		}
-	}
-	return swapped
 }
